39cc1aaa79c0c499d4ca4d5d3057a41ac2bb48c3
or18 <119738717+or18@users.noreply.github.com>
2024-06-24 07:57:57 +0900

    Update COLL.py

--- examples/COLL.py
+++ examples/COLL.py
@@ -4,12 +4,12 @@ import RubiksSolver.move as mv
 #COLL search (using only URF)
 scramble="B2 U2 R2 D B2 R2 U2 R2 D' F2 L' B' R' B D L D F L"
 scramble=sv.solve_F2L(scramble, "z2", True, True, False, True, 14, False, 1, "xxxcross", mv.move_UDLRFB)
-scramble=sv.solve_LL_substep(scramble, "", False, False, False, True, 14, False, 1, "VHLS", mv.move_URF)
+scramble=sv.solve_LL_substep(scramble, "", False, False, False, True, 14, False, 1, "ZBLS", mv.move_URF)
 scramble=sv.solve_LL_substep(scramble, "", True, True, False, True, 14, True, 1, "COLL", mv.move_URF)
 
 """
 z2 L' R2 B' L' B' U' B D2 B' D' B2 L2  // xxxcross No.1
-R U' R2 F R F2 U' F U R U R'  // VHLS No.1
+R U' R2 F R F2 U' F U R U R'  // ZBLS No.1
 1: U R' U2 R U F R' U R U' F'
 2: U R' U2 R U F R' U R U' F' U
 3: U R' U2 R U F R' U R U' F' U2
